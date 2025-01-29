4543a15bfb19ce5ea663af8a734f65e874f50390
chrehall68 <chrehall68@gmail.com>
2025-01-28 16:38:18 -0800

    reset status colors

--- bot/cogs/monitor.py
+++ bot/cogs/monitor.py
@@ -319,7 +319,7 @@ class Monitor(commands.Cog):
         embed = discord.Embed(
             title=f"{'' if good else 'not '}functioning Properly".title(),
             description="",
-            color=discord.Colour.red() if good else discord.Colour.green(),
+            color=discord.Colour.green() if good else discord.Colour.red(),
         )
         embed.description = textwrap.dedent(
             f"""
