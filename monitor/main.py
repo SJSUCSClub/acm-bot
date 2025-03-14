3bedf25a29e129e6876849369f7ad795c155128d
rtk0c <mail.tianyig@gmail.com>
2025-03-14 15:54:17 -0700

    cleaned up logic

--- monitor/main.py
+++ monitor/main.py
@@ -13,57 +13,61 @@ class StatusUpdater:
     def __init__(self):
         self.vals = dotenv.dotenv_values()
         self.last_openness = None
-        self.last_attempt_failed = False
+        self.last_tcp_attempt_failed = False
 
     def __call__(self, open: bool):
         if self.last_openness != open:
             self.last_openness = open
-            self.send_update(open)
 
-    def send_update(self, open: bool):
+            vals = dotenv.dotenv_values()
+
+            # TODO rename this to DOOR_ADDR "host:port" pair, it's not a (colloquial) URL!
+            self.send_tcp_update(vals["DOOR_URL"], int(vals["DOOR_PORT"]), open)
+
+            endpoint = vals.get("DOOR_HTTP_ENDPOINT", None)
+            if endpoint is not None:
+                self.send_http_update(endpoint, open)
+
+    def send_tcp_update(self, host: str, port: int, open: bool):
         """
         Post a status update to the server
 
         Arguments:
-            - open: bool - whether or not the door is currently open
+            - open: bool - whether the door is currently open
         """
 
         s = socket.socket()
-        vals = dotenv.dotenv_values()
         try:
-            # TODO rename this to DOOR_ADDR "host:port" pair, it's not a (colloquial) URL!
-            s.connect((vals["DOOR_URL"], int(vals["DOOR_PORT"])))
+            s.connect((host, port))
         except ConnectionRefusedError:
-            if not self.last_attempt_failed:
+            if not self.last_tcp_attempt_failed:
                 logger.info(
                     "Failed to connect to the server since %s",
                     datetime.now().isoformat(),
                 )
-                self.last_attempt_failed = True
+                self.last_tcp_attempt_failed = True
             return
 
         # make sure we sent the value
         msg = str(open).encode()
         assert s.send(msg) == len(msg)
-        if self.last_attempt_failed:
-            self.last_attempt_failed = False
+        if self.last_tcp_attempt_failed:
+            self.last_tcp_attempt_failed = False
             logger.info(
                 "Able to connect to the server since %s", datetime.now().isoformat()
             )
 
-        endpoint = vals.get("DOOR_HTTP_ENDPOINT", None)
-        if endpoint is not None:
-            try:
-                status_text = 'open' if open else 'closed'
-                headers = {"Content-Type": "text/plain"}
-                r = requests.post(endpoint, headers=headers, data=status_text)
-                r.raise_for_status()
-            except (requests.ConnectionError, requests.HTTPError,):
-                logger.info(
-                    "Failed to send to HTTP endpoint %s",
-                    endpoint
-                )
-                # Do not set last_attempt_failed, this is an optional secondary mechanism
+    def send_http_update(self, endpoint: str, open: bool):
+        try:
+            headers = {"Content-Type": "text/plain"}
+            r = requests.post(endpoint, headers=headers, data=("open" if open else "closed"))
+            r.raise_for_status()
+        except (requests.ConnectionError, requests.HTTPError):
+            logger.info(
+                "Failed to send to HTTP endpoint %s",
+                endpoint
+            )
+            # Do not set last_attempt_failed, this is an optional secondary mechanism
 
 def main():
     # configure root logger
