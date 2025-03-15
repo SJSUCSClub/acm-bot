294a59f7faffa6e103d9d9a8a27772b6081f51e2
rtk0c <mail.tianyig@gmail.com>
2025-03-14 18:09:39 -0700

    add lastUpdated record and display

--- contrib/livestat_aws_lambda/index.mjs
+++ contrib/livestat_aws_lambda/index.mjs
@@ -25,60 +25,64 @@ async function fetchStats(services) {
   return res.Responses[DYNAMO_TABLE];
 }
 
+function renderDateString(dateStr) {
+  const date = new Date(dateStr);
+  // Timezone is set by TZ environment variable, which will get picked up by NodeJS (and libc) automatically
+  return date.toLocaleString("en-US", { timeZoneName: "shortOffset" });
+}
+
 function renderStats(stats, format) {
   let contentType, body;
 
   switch (format) {
   case "json":
     contentType = "application/json";
-    body = JSON.stringify(stats.map(service => service.status));
+    body = JSON.stringify(Object.fromEntries(stats.map(service => {
+      const {id, ...rest} = service;
+      return [id, rest];
+    })));
     break;
   case "html":
     contentType = "text/html";
-    const headerRow = `<tr><th>Service</th><th>Status</th></tr>`;
-    const rows = stats.map(service => `<tr><td>${service.id}</td><td>${service.status}</td></tr>`);
-    body = `<html><body><table>${headerRow}${rows}</table></body></html>`;
+    const styles = `<style>table { border-collapse: collapse; } th, td { border: 1px solid black; padding: 0.5em; }</style>`;
+    const headerRow = `<tr><th>Service</th><th>Status</th><th>Last Updated</th></tr>`;
+    const rows = stats.map(service => `<tr><td>${service.id}</td><td>${service.status}</td><td>${renderDateString(service.lastUpdated)}</td></tr>`);
+    body = `<html><head>${styles}</head><body><table>${headerRow}${rows}</table></body></html>`;
     break;
   case "plaintext":
     // TODO better way to format this?
     contentType = "text/plain";
-    body = stats.map(service => `# ${service.id}\n${service.status}\n`).join("\n");
+    body = stats.map(service => `${service.id}\nsince: ${renderDateString(service.lastUpdated)}\nis: ${service.status}\n`).join("\n");
     break;
   default:
-    return {
-      statusCode: 400,
-      body: "Invalid format",
-    }
+    return { statusCode: 400, body: "Invalid format" };
   }
 
   return {
     statusCode: 200,
     headers: {
       "Content-Type": contentType,
+      "Cache-Control": "max-age=300", // Cache up to 5 minutes
     },
     body: body,
   }
 }
 
 async function updateServiceStat(id, status) {
-  try {
-    await dynamo.send(new UpdateCommand({
-      TableName: DYNAMO_TABLE,
-      Key: { id },
-      UpdateExpression: "set #status = :status",
-      ExpressionAttributeNames: {
-        "#status": "status",
-      },
-      ExpressionAttributeValues: {
-        ":status": status,
-      },
-    }));
-  } catch (err) {
-    console.error(err);
-    return {
-      statusCode: 500,
-    };
-  }
+  // Let errors propagate into logs and a 500
+  await dynamo.send(new UpdateCommand({
+    TableName: DYNAMO_TABLE,
+    Key: { id },
+    UpdateExpression: "set #status = :status, #lastUpdated = :now",
+    ExpressionAttributeNames: {
+      "#status": "status",
+      "#lastUpdated": "lastUpdated",
+    },
+    ExpressionAttributeValues: {
+      ":status": status,
+      ":now": new Date().toISOString(),
+    },
+  }));
 
   return {
     statusCode: 200,
@@ -93,6 +97,7 @@ async function newService(id) {
       Item: {
         id,
         status: '',
+        lastUpdated: new Date().toISOString(),
       },
       // Don't override an existing service
       ConditionExpression: "attribute_not_exists(id)",
@@ -100,10 +105,7 @@ async function newService(id) {
     console.log(`new-service: "${id}" added`);
   } catch (e) {
     if (e.name === "ConditionalCheckFailedException") {
-      return {
-        statusCode: 409,
-        body: "Service already exists",
-      };
+      return { statusCode: 409, body: "Service already exists" };
     }
 
     // Exception not recognized, fail dramatically
@@ -121,11 +123,20 @@ async function deleteService(id) {
 
 export const handler = async (event, context) => {
   const req = event.requestContext.http;
-  const params = event.queryStringParameters;
+  const params = event.queryStringParameters || {};
 
   switch (req.method) {
   case "GET":
     if (req.path === "/") {
+      // Validation
+      if (!params.services) {
+        return {
+          statusCode: 400,
+          body: "Missing parameter 'services'",
+        };
+      }
+
+      // Respond
       const services = params.services.split(",");
       const stats = await fetchStats(services);
       return renderStats(stats, params.format || 'json');
@@ -133,7 +144,7 @@ export const handler = async (event, context) => {
     break;
   case "POST":
     if (params.token !== MASTER_TOKEN) {
-      throw new Error("Invalid token");
+      return { statusCode: 401, body: "Invalid token" };
     }
 
     switch (req.path) {
@@ -145,7 +156,7 @@ export const handler = async (event, context) => {
     break;
   case "DELETE":
     if (params.token !== MASTER_TOKEN) {
-      throw new Error("Invalid token");
+      return { statusCode: 401, body: "Invalid token" };
     }
 
     switch (req.path) {
@@ -154,8 +165,5 @@ export const handler = async (event, context) => {
     }
   }
 
-  return {
-    statusCode: 400,
-    body: "Invalid request",
-  };
+  return { statusCode: 400, body: "Invalid request" };
 };
